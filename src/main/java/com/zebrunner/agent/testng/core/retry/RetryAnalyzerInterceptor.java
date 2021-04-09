11b8f5ca7230999ad0efc988057e5f9d940c27e8
Sergei Zagriychuk <sergeizagriychuk@gmail.com>
2021-04-09 17:37:05 +0300

    check for a known issue was added to RetryAnalyzerInterceptor

--- src/main/java/com/zebrunner/agent/testng/core/retry/RetryAnalyzerInterceptor.java
+++ src/main/java/com/zebrunner/agent/testng/core/retry/RetryAnalyzerInterceptor.java
@@ -1,12 +1,16 @@
 package com.zebrunner.agent.testng.core.retry;
 
+import com.zebrunner.agent.core.registrar.TestRunRegistrar;
 import com.zebrunner.agent.testng.listener.RetryService;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestContext;
 import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import org.testng.internal.InstanceCreator;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,9 +18,15 @@ import java.util.concurrent.ConcurrentHashMap;
  * Retry analyzer interceptor that keeps track of invocation index and checks if all test method retries has
  * been executed. Method with retries is only registered once to Zebrunner
  */
+@Slf4j
 public class RetryAnalyzerInterceptor implements IRetryAnalyzer {
 
     private static final Map<String, IRetryAnalyzer> RETRY_ANALYZER_KEY_TO_IDENTITY = new ConcurrentHashMap<>();
+    private final TestRunRegistrar registrar;
+
+    public RetryAnalyzerInterceptor() {
+        this.registrar = TestRunRegistrar.getInstance();
+    }
 
     @Override
     public boolean retry(ITestResult result) {
@@ -25,6 +35,19 @@ public class RetryAnalyzerInterceptor implements IRetryAnalyzer {
 
         IRetryAnalyzer retryAnalyzer = getOriginalRetryAnalyzer(result);
         boolean needRetry = retryAnalyzer.retry(result);
+
+        // checking whether there are some known issues were mapped for stacktrace and skipping retry logic in such case
+        StringWriter sw = new StringWriter();
+        try(PrintWriter pw = new PrintWriter(sw)) {
+            result.getThrowable().printStackTrace(pw);
+        }
+        if(registrar.isKnownIssueAttachedToTest(sw.toString())) {
+            log.info("Known issue is attached to test for current failure stacktrace. Hence skipping retry logic");
+            needRetry = false;
+        } else {
+            log.info("No known issues are attached");
+        }
+
         if (needRetry) {
             RetryService.setRetryStarted(method, context);
         } else {
