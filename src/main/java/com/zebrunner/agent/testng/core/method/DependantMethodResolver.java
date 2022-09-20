d038629cce209f381ad1ad836e57ae20793c819f
Dima Kazak <dkazak@solvd.com>
2022-09-20 17:15:08 +0300

    Resolve dependant methods created through different factories

--- src/main/java/com/zebrunner/agent/testng/core/method/DependantMethodResolver.java
+++ src/main/java/com/zebrunner/agent/testng/core/method/DependantMethodResolver.java
@@ -1,26 +1,35 @@
 package com.zebrunner.agent.testng.core.method;
 
+import com.zebrunner.agent.testng.core.FactoryInstanceHolder;
 import org.testng.IMethodInstance;
 import org.testng.ITestNGMethod;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.Stack;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public final class DependantMethodResolver {
 
+    /**
+     * Resolve dependant methods using tree Depth-first search. There can be both direct dependant methods and dependant groups.
+     * In such cases method resolves direct dependant methods and after resolves dependant group methods.
+     *
+     * @param methodsSuperSet the whole set of methods
+     * @param methods         methods that must be resolved with their dependant methods
+     * @return dependant methods
+     */
     public static Set<IMethodInstance> resolve(Collection<IMethodInstance> methodsSuperSet, Collection<IMethodInstance> methods) {
-        Map<String, IMethodInstance> nameToMethod = DependantMethodResolver.collectMethodsByName(methodsSuperSet);
+        Map<String, Set<IMethodInstance>> nameToMethods = DependantMethodResolver.collectMethodsByName(methodsSuperSet);
         Set<IMethodInstance> dependantMethods = methods.stream()
                                                        .map(IMethodInstance::getMethod)
-                                                       .map(method -> DependantMethodResolver.getDependantMethods(nameToMethod, method))
+                                                       .map(method -> DependantMethodResolver.getDependantMethods(nameToMethods, method))
                                                        .flatMap(Collection::stream)
                                                        .collect(Collectors.toSet());
 
@@ -35,44 +44,65 @@ public final class DependantMethodResolver {
         return dependantMethods;
     }
 
-    private static Map<String, IMethodInstance> collectMethodsByName(Collection<IMethodInstance> methods) {
+    /**
+     * Collect methods by qualified name. Qualified name = testClass name + "." + testMethod name.
+     * Notice: tests with same name can belong to different factory instances.
+     *
+     * @return map where key is method's qualified name and value is set with belonging to this name test methods.
+     */
+    private static Map<String, Set<IMethodInstance>> collectMethodsByName(Collection<IMethodInstance> methods) {
         return methods.stream()
-                      .collect(Collectors.toMap(instance -> instance.getMethod().getQualifiedName(), Function.identity()));
+                      .collect(
+                              Collectors.groupingBy(
+                                      instance -> instance.getMethod().getQualifiedName(),
+                                      Collectors.toSet()
+                              )
+                      );
     }
 
     private static Map<String, Set<IMethodInstance>> collectMethodsByDependedGroups(Collection<IMethodInstance> methods) {
-        Map<String, Set<IMethodInstance>> groupToMethod = new HashMap<>();
+        Map<String, Set<IMethodInstance>> groupToMethods = new HashMap<>();
 
         methods.stream()
                .map(IMethodInstance::getMethod)
                .flatMap(method -> Arrays.stream(method.getGroups()))
-               .forEach(group -> groupToMethod.put(group, new HashSet<>()));
+               .forEach(group -> groupToMethods.put(group, new HashSet<>()));
 
         methods.forEach(instance -> Arrays.stream(instance.getMethod().getGroups())
-                                          .map(groupToMethod::get)
+                                          .map(groupToMethods::get)
                                           .filter(Objects::nonNull)
                                           .forEach(groupMethods -> groupMethods.add(instance)));
-        return groupToMethod;
+        return groupToMethods;
     }
 
-    private static Set<IMethodInstance> getDependantMethods(Map<String, IMethodInstance> methods, ITestNGMethod method) {
+    private static Set<IMethodInstance> getDependantMethods(Map<String, Set<IMethodInstance>> methods, ITestNGMethod method) {
+        Set<String> resolvedMethods = new HashSet<>();
         Set<IMethodInstance> dependantMethods = new HashSet<>();
 
-        Stack<String> dependantMethodNames = new Stack<>();
+        Stack<String> methodsToResolve = new Stack<>();
         // init stack with first level dependant methods
-        Set<String> dependantMethodNamesBatch = Arrays.stream(method.getMethodsDependedUpon())
+        Set<String> nextMethodsToResolveBatch = Arrays.stream(method.getMethodsDependedUpon())
                                                       .collect(Collectors.toSet());
-        dependantMethodNames.addAll(dependantMethodNamesBatch);
+        methodsToResolve.addAll(nextMethodsToResolveBatch);
 
-        while (!dependantMethodNames.isEmpty()) {
-            String dependantMethodName = dependantMethodNames.pop();
-            IMethodInstance dependantMethod = methods.get(dependantMethodName);
+        while (!methodsToResolve.isEmpty()) {
+            String dependantMethodName = methodsToResolve.pop();
+            resolvedMethods.add(dependantMethodName);
+            Set<IMethodInstance> methodsWithSameFactoryInstance = methods.getOrDefault(dependantMethodName, Collections.emptySet())
+                                                                         .stream()
+                                                                         .filter(dependantMethod -> belongsToTheSameFactoryInstance(dependantMethod.getMethod(), method))
+                                                                         .collect(Collectors.toSet());
 
-            if (dependantMethod != null && !dependantMethods.contains(dependantMethod)) {
-                dependantMethods.add(dependantMethod);
-                dependantMethodNamesBatch = Arrays.stream(dependantMethod.getMethod().getMethodsDependedUpon())
-                                                  .collect(Collectors.toSet());
-                dependantMethodNames.addAll(dependantMethodNamesBatch);
+            if (!methodsWithSameFactoryInstance.isEmpty()) {
+                dependantMethods.addAll(methodsWithSameFactoryInstance);
+                nextMethodsToResolveBatch = methodsWithSameFactoryInstance.stream()
+                                                                          .map(IMethodInstance::getMethod)
+                                                                          .map(ITestNGMethod::getMethodsDependedUpon)
+                                                                          .flatMap(Arrays::stream)
+                                                                          // filter out methods that are already resolved
+                                                                          .filter(methodName -> !resolvedMethods.contains(methodName))
+                                                                          .collect(Collectors.toSet());
+                methodsToResolve.addAll(nextMethodsToResolveBatch);
             }
         }
 
@@ -83,31 +113,38 @@ public final class DependantMethodResolver {
         Set<String> resolvedGroups = new HashSet<>();
         Set<IMethodInstance> dependantMethods = new HashSet<>();
 
-        Stack<String> dependantGroupNames = new Stack<>();
+        Stack<String> groupsToResolve = new Stack<>();
         // init stack with first level dependant groups
-        Set<String> dependantGroupNamesBatch = Arrays.stream(method.getGroupsDependedUpon())
+        Set<String> nextGroupsToResolveBatch = Arrays.stream(method.getGroupsDependedUpon())
                                                      .collect(Collectors.toSet());
-        dependantGroupNames.addAll(dependantGroupNamesBatch);
+        groupsToResolve.addAll(nextGroupsToResolveBatch);
 
-        while (!dependantGroupNames.isEmpty()) {
-            String dependantGroup = dependantGroupNames.pop();
+        while (!groupsToResolve.isEmpty()) {
+            String dependantGroup = groupsToResolve.pop();
             resolvedGroups.add(dependantGroup);
-            Set<IMethodInstance> dependantGroupMethods = groups.get(dependantGroup);
+            Set<IMethodInstance> methodsWithSameFactoryInstance = groups.getOrDefault(dependantGroup, Collections.emptySet())
+                                                                        .stream()
+                                                                        .filter(dependantMethod -> belongsToTheSameFactoryInstance(dependantMethod.getMethod(), method))
+                                                                        .collect(Collectors.toSet());
 
-            if (dependantGroupMethods != null) {
-                dependantMethods.addAll(dependantGroupMethods);
-                dependantGroupNamesBatch = dependantGroupMethods.stream()
-                                                                .map(IMethodInstance::getMethod)
-                                                                .map(ITestNGMethod::getGroupsDependedUpon)
-                                                                .flatMap(Arrays::stream)
-                                                                // filter out groups that are already resolved
-                                                                .filter(group -> !resolvedGroups.contains(group))
-                                                                .collect(Collectors.toSet());
-                dependantGroupNames.addAll(dependantGroupNamesBatch);
+            if (!methodsWithSameFactoryInstance.isEmpty()) {
+                dependantMethods.addAll(methodsWithSameFactoryInstance);
+                nextGroupsToResolveBatch = methodsWithSameFactoryInstance.stream()
+                                                                         .map(IMethodInstance::getMethod)
+                                                                         .map(ITestNGMethod::getGroupsDependedUpon)
+                                                                         .flatMap(Arrays::stream)
+                                                                         // filter out groups that are already resolved
+                                                                         .filter(group -> !resolvedGroups.contains(group))
+                                                                         .collect(Collectors.toSet());
+                groupsToResolve.addAll(nextGroupsToResolveBatch);
             }
         }
 
         return dependantMethods;
+    }
+
+    private static boolean belongsToTheSameFactoryInstance(ITestNGMethod method1, ITestNGMethod method2) {
+        return FactoryInstanceHolder.getInstanceIndex(method1) == FactoryInstanceHolder.getInstanceIndex(method2);
     }
 
 }
