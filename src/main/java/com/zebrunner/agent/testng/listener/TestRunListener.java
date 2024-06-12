9794e4df83429f5da210e6d394bb41191f0507e2
Andrei Kamarouski <akamarouski@solvd.com>
2024-06-12 11:12:17 +0200

    feature(TestRunListener): register before class headless start

--- src/main/java/com/zebrunner/agent/testng/listener/TestRunListener.java
+++ src/main/java/com/zebrunner/agent/testng/listener/TestRunListener.java
@@ -95,6 +95,10 @@ public class TestRunListener extends RerunAwareListener implements ISuiteListene
         if (testMethod instanceof ConfigurationMethod) {
             ConfigurationMethod configurationMethod = (ConfigurationMethod) testMethod;
 
+            if (configurationMethod.isBeforeClassConfiguration()) {
+                adapter.registerHeadlessTestStart(tr, tm);
+            }
+
             if (configurationMethod.isBeforeMethodConfiguration()) {
                 adapter.registerHeadlessTestStart(tr, tm);
             }
