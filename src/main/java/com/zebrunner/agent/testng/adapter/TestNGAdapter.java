cf11802741eab99391381b3aea7c9c88b6ad89d9
edovnar <edovnar@solvd.com>
2025-02-06 17:31:11 +0100

    Added Blocked test status

--- src/main/java/com/zebrunner/agent/testng/adapter/TestNGAdapter.java
+++ src/main/java/com/zebrunner/agent/testng/adapter/TestNGAdapter.java
@@ -19,6 +19,7 @@ import java.util.stream.Collectors;
 
 import com.zebrunner.agent.core.config.ConfigurationHolder;
 import com.zebrunner.agent.core.config.provider.SystemPropertiesConfigurationProvider;
+import com.zebrunner.agent.core.exception.BlockedTestException;
 import com.zebrunner.agent.core.registrar.RunContextHolder;
 import com.zebrunner.agent.core.registrar.TestRunRegistrar;
 import com.zebrunner.agent.core.registrar.descriptor.Status;
@@ -243,7 +244,11 @@ public class TestNGAdapter {
             OffsetDateTime endedAt = ofMillis(endedAtMillis);
             String errorMessage = ExceptionUtils.getStacktrace(testResult.getThrowable());
 
-            TestFinishDescriptor result = new TestFinishDescriptor(Status.FAILED, endedAt, errorMessage);
+            Status status = testResult.getThrowable() instanceof BlockedTestException
+                    ? Status.BLOCKED
+                    : Status.FAILED;
+
+            TestFinishDescriptor result = new TestFinishDescriptor(status, endedAt, errorMessage);
             registrar.registerTestFinish(id, result);
         } else {
             log.debug("TestNGAdapter -> registerFailedTestFinish: retry is NOT finished");
